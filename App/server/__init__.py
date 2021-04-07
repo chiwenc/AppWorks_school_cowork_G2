e997735cb3c1e61f1b943acd4997e88061d7b5af
Arthur Lin <gn01168178@yahoo.com.tw>
2021-04-07 15:08:18 +0800

    add error handling

--- App/server/__init__.py
+++ App/server/__init__.py
@@ -6,7 +6,7 @@ import os
 import pymysql
 from flask_jwt_extended import JWTManager
 from waitress import serve
-
+from werkzeug.exceptions import HTTPException
 from sqlalchemy.ext.declarative import declared_attr, has_inherited_table
 
 class BaseModel(Model):
@@ -20,5 +20,15 @@ migrate = Migrate(app, db)
 
 jwt = JWTManager()
 jwt.init_app(app)
+
+@app.errorhandler(404)
+def server_error(error):
+    return "Page not found", 404
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    if isinstance(e, HTTPException):
+        return e
+    return "Internal Server Error", 500
 
 from server.controllers import product_controller, user_controller
