e30cf5bd56696558244b897cd51b9c1ec25a2dff
Arthur Lin <gn01168178@yahoo.com.tw>
2021-04-12 08:49:21 +0800

    add full UA

--- Recommendation/crawler.py
+++ Recommendation/crawler.py
@@ -23,15 +23,24 @@ conn = pymysql.connect(
 )
 
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0',
-    'Accept-Language': 'en-US'
+    'authority': 'scrapeme.live',
+    'dnt': '1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site': 'none',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-user': '?1',
+    'sec-fetch-dest': 'document',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
 }
+
 cookies = {'enwiki_session': '17ab96bd8ffbe8ca58a78657a918558'}
 
 def get_items():
     cursor = conn.cursor()
     cursor.execute(
-        "SELECT DISTINCT(item1_id) AS item_id FROM similarity_model LIMIT 3000, 12"
+        "SELECT DISTINCT(item1_id) AS item_id FROM similarity_model LIMIT 5000, 12"
     )
     conn.commit()
     return cursor.fetchall()
