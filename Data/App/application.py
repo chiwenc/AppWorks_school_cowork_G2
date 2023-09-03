d427c9eaa0c228907bffe5dc714a81a327e06ef7
Chuan <kirk336neo@gmail.com>
2023-09-03 23:34:28 +0800

    feat ($ compile): Finish Checkout API
    - API: Check out

--- Data/App/application.py
+++ Data/App/application.py
@@ -7,3 +7,6 @@ if __name__ == "__main__":
     #     serve(app, port=3000)
     # else:
         app.run("0.0.0.0", port=8000, debug=True)
+
+    # run: waitress-serve --host 127.0.0.1 application:app
+    # run: waitress-serve --host 0.0.0.0 application:app
